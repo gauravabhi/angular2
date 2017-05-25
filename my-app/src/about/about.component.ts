2419df76debc9085141b0c3c7582b50bf92819d3
abhishek <gaurav.mca14@gmail.com>
2017-05-25 23:18:08 +0530

    update for for

--- my-app/src/about/about.component.ts
+++ my-app/src/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
     templateUrl:'./about.component.html'
 })
 
-export class AboutComponent{}
+export class AboutComponent implements OnInit{
+  
+   ngOnInit(){}
+}
